b2cee046066cb7b04dbe88a79189e46abe3f9a59
Amine Boularbah <boularbah.amine2000@gmail.com>
2020-11-08 14:56:53 +0100

    Project setup

---
+++ backend/AWSImageApi/src/main/java/com/example/imageApi/AWSImageApi/AwsImageApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageApi.AWSImageApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageApiApplication.class, args);
+	}
+
+}
---
+++ backend/AWSImageApi/src/test/java/com/example/imageApi/AWSImageApi/AwsImageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageApi.AWSImageApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
